af395d8f7caeda48f6c5037d17320f5a9897f8fb
zia.haider <86730092+zianhaidernaqvi@users.noreply.github.com>
2023-01-07 15:15:14 +0530

    first commit

---
+++ src/main/java/com/sampleProject/sampleproject/SampleProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sampleProject.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApplication.class, args);
+		System.out.println("Hiiiii");
+	}
+
+}
